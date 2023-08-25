c53b990a2e70b7ab169947df126f99cc4629d30f
yohasebe <yohasebe@gmail.com>
2023-08-25 15:22:47 +0900

    sha 256 is created from the version specific file

--- whisper-stream.rb
+++ whisper-stream.rb
@@ -1,7 +1,7 @@
 class WhisperStream < Formula
   desc "Bash script to transcribe spoken audio into text using Whisper API"
   homepage "https://github.com/yohasebe/whisper-stream"
-  url "https://raw.githubusercontent.com/yohasebe/whisper-stream/main/whisper-stream"
+  url "https://raw.githubusercontent.com/yohasebe/whisper-stream/6ba62b610001d17dbb3cca209194184ac1052cb4/whisper-stream"
   version "1.0.3"
   sha256 "8833409913c3d2f04fea97cc0529fb4787615581959c3b44982f4f8a9f9e256b"
   license "MIT"
