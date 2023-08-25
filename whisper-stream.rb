378272e526c3c12dbc810ed871654421bb921e56
yohasebe <yohasebe@gmail.com>
2023-08-25 16:28:59 +0900

    for version 1.0.5

--- whisper-stream.rb
+++ whisper-stream.rb
@@ -3,7 +3,7 @@ class WhisperStream < Formula
   homepage "https://github.com/yohasebe/whisper-stream"
   url "https://github.com/yohasebe/whisper-stream/blob/373af760df4853f9e82ca663bb9acb2b18224bcc/whisper-stream"
   version "1.0.5"
-  sha256 "2f1cb79d48fc5769dcaf1433bd783223c120a027b9927dcab6f7dd6842395f84"
+  sha256 "b97bfeabb2e830661e910054e5c782510c57cf1c13c81cc086774c8df98eb085"
   license "MIT"
 
   depends_on "jq"
