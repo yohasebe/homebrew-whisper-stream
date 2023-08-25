e23a3722921c64951a40e206db2756f743878b3e
Yoichiro Hasebe <yohasebe@gmail.com>
2023-08-25 14:37:31 +0900

    a file extension has been removed

--- whisper-stream.rb
+++ whisper-stream.rb
@@ -1,7 +1,7 @@
 class WhisperStream < Formula
   desc "Bash script to transcribe spoken audio into text using Whisper API"
   homepage "https://github.com/yohasebe/whisper-stream"
-  url "https://raw.githubusercontent.com/yohasebe/whisper-stream/main/whisper-stream.sh"
+  url "https://raw.githubusercontent.com/yohasebe/whisper-stream/main/whisper-stream"
   version "1.0.3"
   sha256 "8833409913c3d2f04fea97cc0529fb4787615581959c3b44982f4f8a9f9e256b"
   license "MIT"
@@ -12,7 +12,7 @@ class WhisperStream < Formula
   uses_from_macos "curl"
 
   def install
-    bin.install "whisper-stream" => "whisper-stream"
+    bin.install "whisper-stream"
   end
 
   test do
