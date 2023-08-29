d4bfaca40499c5d1dd3d7e6cc5976922a2ba92ea
yohasebe <yohasebe@gmail.com>
2023-08-29 16:55:48 +0900

    1.0.6

--- whisper-stream.rb
+++ whisper-stream.rb
@@ -1,9 +1,8 @@
 class WhisperStream < Formula
   desc "Bash script to transcribe spoken audio into text using Whisper API"
-  homepage "https://github.com/yohasebe/whisper-stream"
-  url "https://github.com/yohasebe/whisper-stream/archive/373af760df4853f9e82ca663bb9acb2b18224bcc.zip"
-  version "1.0.5"
-  sha256 "f1a9857914a307d6c84f56029dfed245c22d5e7919b690efaf8c3a530a500d4f"
+  homepage "https://github.com/yohasebe/whisper-stream/archive/ec300df942d0bb086e455095543986686aa0a9e8.zip"
+  version "1.0.6"
+  sha256 "671da9aa1ac5632d577e7945821812c14ad13ca777c6985ee43ad86f147efded"
   license "MIT"
 
   depends_on "jq"
