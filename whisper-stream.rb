02440f22f4150589a90c6422c840dc98b82d960e
Yoichiro Hasebe <yohasebe@gmail.com>
2026-05-11 09:44:45 +0900

    3.1.0

--- whisper-stream.rb
+++ whisper-stream.rb
@@ -1,9 +1,9 @@
 class WhisperStream < Formula
   desc "Bash script for real-time speech-to-text via OpenAI API or local whisper.cpp"
   homepage "https://github.com/yohasebe/whisper-stream"
-  url "https://github.com/yohasebe/whisper-stream/archive/8a194c37f6ed4128f34b0d56d8bed9b0cd97369c.zip"
-  version "3.0.0"
-  sha256 "e4e975436551daf8ea5cd6e5b9c010e381acd1b0696f1acefa738412b7db1b5c"
+  url "https://github.com/yohasebe/whisper-stream/archive/f24f14d41ad0efce6a5061eb6298d5b8743d7354.zip"
+  version "3.1.0"
+  sha256 "dcc5ee1ebe333c3e839e17d3e8770100a97af29619dff91cfa7f940a4c1ab23f"
   license "MIT"
 
   depends_on "jq"
